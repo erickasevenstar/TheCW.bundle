dd1f43f0cc1a77f8591dc7b1dabaff5396292f59
sander1 <sander@plexapp.com>
2012-12-07 01:08:09 +0100

    Update season/episode regex; update 'details' xpath.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,17 +1,17 @@
-RE_SEASON_EP = Regex("Season (?P<season>[0-9]+), EP. (?P<episode>[0-9]+)")
+RE_SEASON_EP = Regex('Season (?P<season>\d+), Ep. (?P<episode>\d+)')
 
-CW_ROOT = "http://www.cwtv.com"
-CW_SHOWS_LIST = "http://www.cwtv.com/shows"
-EP_URL = "http://www.cwtv.com/cw-video/"
+CW_ROOT = 'http://www.cwtv.com'
+CW_SHOWS_LIST = 'http://www.cwtv.com/shows'
+EP_URL = 'http://www.cwtv.com/cw-video/'
 
 ####################################################################################################
 def Start():
 
 	Plugin.AddPrefixHandler("/video/thecw", MainMenu, "The CW", "icon-default.png", "art-default.jpg")
-	Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
+	Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
 	ObjectContainer.art = R('art-default.jpg')
 	ObjectContainer.title1 = 'The CW'
-	DirectoryObject.thumb = R("icon-default.png")
+	DirectoryObject.thumb = R('icon-default.png')
 
 ####################################################################################################
 def MainMenu():
@@ -20,38 +20,38 @@ def MainMenu():
 	content = HTML.ElementFromURL(CW_SHOWS_LIST)
 
 	for item in content.xpath('//div[@class="shows-current"]//a[@class="hublink"]'):
-		link = item.get('href').replace("/shows","")
+		link = item.get('href').replace('/shows', '')
 		link = EP_URL + link
-		link = link.replace('video//','video/')
-		title = item.xpath(".//img")[0].get('title')
-		title = title.replace("-"," ").title()
+		link = link.replace('video//', 'video/')
+		title = item.xpath('.//img')[0].get('title')
+		title = title.replace('-', ' ').title()
 
-		thumb = item.xpath(".//img")[0].get('src')
+		thumb = item.xpath('.//img')[0].get('src')
 		if not thumb.startswith('http://'):
 			thumb = CW_ROOT + thumb
 
-		oc.add(DirectoryObject(key=Callback(EpList, pageUrl=link, title=title), title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback='icon-default.png')))
+		oc.add(DirectoryObject(key=Callback(EpList, url=link, title=title), title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback='icon-default.png')))
 
 	return oc
 
 ####################################################################################################
-def EpList(pageUrl, title):
+def EpList(url, title):
 
 	oc = ObjectContainer(title2=title)
-	content = HTML.ElementFromURL(pageUrl)
+	content = HTML.ElementFromURL(url)
 
-	for item in content.xpath('//div[contains(@class,"videowrapped slide full-episodes")]'):
+	for item in content.xpath('//div[contains(@class, "videowrapped slide full-episodes")]'):
 		link = item.xpath('.//a[@class="thumbLink"]')[0].get('href')
 		if not link.startswith('http://'):
 			link = CW_ROOT + link
 
-		thumb = item.xpath(".//img")[0].get('src')
+		thumb = item.xpath('.//img')[0].get('src')
 		if not thumb.startswith('http://'):
 			thumb = CW_ROOT + thumb
 
-		video_title = item.xpath('.//p[@class="et"]')[0].text.title()
-		details = item.xpath('.//p[@class="d2"]/text()')
-		summary = item.xpath('.//p[@class="d3"]')[0].text
+		video_title = item.xpath('.//p[@class="et"]')[0].text.title().replace("'S", "'s")
+		details = item.xpath('.//p[@class="d2"]//text()')
+		summary = item.xpath('.//p[@class="d3"]')[0].text.split(' Watch free')[0]
 
 		try:
 			season_and_episode = RE_SEASON_EP.search(details[0]).groupdict()
@@ -62,7 +62,7 @@ def EpList(pageUrl, title):
 			epIndex = None
 
 		try:
-			date = Datetime.ParseDate(details[1].split('Original Air Date:')[1]).date()
+			date = Datetime.ParseDate(details[1].split('Original Air Date: ')[1]).date()
 		except:
 			date = None
 
