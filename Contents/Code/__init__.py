04803dae849227df5402a95c0111d5a9879e0a1a
sander1 <sander@plexapp.com>
2013-04-28 15:29:40 +0200

    Add routes, use hosted resources.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -2,64 +2,62 @@ RE_SEASON_EP = Regex('Season (?P<season>\d+), Ep. (?P<episode>\d+)')
 
 CW_ROOT = 'http://www.cwtv.com'
 CW_SHOWS_LIST = 'http://www.cwtv.com/shows'
-EP_URL = 'http://www.cwtv.com/cw-video/'
+EP_URL = 'http://www.cwtv.com/cw-video'
 
 ####################################################################################################
 def Start():
 
-	Plugin.AddPrefixHandler("/video/thecw", MainMenu, "The CW", "icon-default.png", "art-default.jpg")
-	Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
-	ObjectContainer.art = R('art-default.jpg')
 	ObjectContainer.title1 = 'The CW'
-	DirectoryObject.thumb = R('icon-default.png')
 
 ####################################################################################################
+@handler('/video/thecw', 'The CW')
 def MainMenu():
 
 	oc = ObjectContainer()
-	content = HTML.ElementFromURL(CW_SHOWS_LIST)
+	html = HTML.ElementFromURL(CW_SHOWS_LIST)
 
-	for item in content.xpath('//div[@class="shows-current"]//a[@class="hublink"]'):
-		link = item.get('href').replace('/shows', '')
-		link = EP_URL + link
-		link = link.replace('video//', 'video/')
-		title = item.xpath('.//img')[0].get('title')
-		title = title.replace('-', ' ').title()
+	for item in html.xpath('//div[@class="shows-current"]//a[@class="hublink"]'):
+		show = item.get('href').split('/')[-1]
+		url = '%s/%s' % (EP_URL, show)
+		title = String.CapitalizeWords(show.replace('-', ' '))
+		thumb = item.xpath('.//img/@src')[0]
 
-		thumb = item.xpath('.//img')[0].get('src')
-		if not thumb.startswith('http://'):
-			thumb = CW_ROOT + thumb
-
-		oc.add(DirectoryObject(key=Callback(EpList, url=link, title=title), title=title, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback='icon-default.png')))
+		oc.add(DirectoryObject(
+			key = Callback(Episodes, url=url, title=title),
+			title = title,
+			thumb = Resource.ContentsOfURLWithFallback(url=thumb)
+		))
 
 	return oc
 
 ####################################################################################################
-def EpList(url, title):
+@route('/video/thecw/episodes')
+def Episodes(url, title):
 
 	oc = ObjectContainer(title2=title)
-	content = HTML.ElementFromURL(url)
+	html = HTML.ElementFromURL(url)
 
-	for item in content.xpath('//div[contains(@class, "videowrapped slide full-episodes")]'):
-		link = item.xpath('.//a[@class="thumbLink"]')[0].get('href')
-		if not link.startswith('http://'):
-			link = CW_ROOT + link
+	for item in html.xpath('//div[contains(@class, "full-episodes")]'):
+		url = item.xpath('./a[@class="thumbLink"]/@href')[0]
+		if not url.startswith('http://'):
+			url = '%s%s' % (CW_ROOT, url)
 
-		thumb = item.xpath('.//img')[0].get('src')
+		thumb = item.xpath('.//img/@src')[0]
 		if not thumb.startswith('http://'):
-			thumb = CW_ROOT + thumb
+			thumb = '%s%s' % (CW_ROOT, thumb)
 
-		video_title = item.xpath('.//p[@class="et"]')[0].text.title().replace("'S", "'s")
+		episode_title = item.xpath('.//p[@class="et"]/text()')[0]
+		summary = item.xpath('.//p[@class="d3"]/text()')[0].split(' Watch free')[0]
+
 		details = item.xpath('.//p[@class="d2"]//text()')
-		summary = item.xpath('.//p[@class="d3"]')[0].text.split(' Watch free')[0]
 
 		try:
 			season_and_episode = RE_SEASON_EP.search(details[0]).groupdict()
 			season = int(season_and_episode['season'])
-			epIndex = int(season_and_episode['episode'])
+			ep_index = int(season_and_episode['episode'])
 		except:
 			season = None
-			epIndex = None
+			ep_index = None
 
 		try:
 			date = Datetime.ParseDate(details[1].split('Original Air Date: ')[1]).date()
@@ -67,9 +65,22 @@ def EpList(url, title):
 			date = None
 
 		if date:
-			oc.add(EpisodeObject(url=link, title=video_title, show=title, index=epIndex, season=season, summary=summary,
-			originally_available_at=date, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback='icon-default.png')))
+			oc.add(EpisodeObject(
+				url = url,
+				title = episode_title,
+				show = title,
+				index = ep_index,
+				season = season,
+				summary = summary,
+				originally_available_at = date,
+				thumb = Resource.ContentsOfURLWithFallback(url=thumb)
+			))
 		else:
-			oc.add(VideoClipObject(url=link, title=video_title, summary=summary, thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback='icon-default.png')))
+			oc.add(VideoClipObject(
+				url = link,
+				title = video_title,
+				summary = summary,
+				thumb = Resource.ContentsOfURLWithFallback(url=thumb)
+			))
 
 	return oc
